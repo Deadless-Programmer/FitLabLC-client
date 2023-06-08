cd045adb7b5dbff77a934dc868bb720122eb388a
Deadless-Programmer <webtuhin@gmail.com>
2023-06-08 14:00:13 +0600

    sign up page social log in done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from './../../providers/AuthProvider';
 
 const SignUp = () => {
-	const { createUser } = useContext(AuthContext);
+	const { createUser, googleSignIn } = useContext(AuthContext);
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
@@ -21,6 +21,18 @@ const SignUp = () => {
 		.catch(error => console.log(error))
 
 	};
+	const googleSignInHandler =()=>{
+		googleSignIn()
+		.then((result) => {
+		  const loggedUser = result.user;
+		  console.log(loggedUser);
+		 
+		  navigete(from, { replace: true });
+		})
+		.catch((error) => {
+		  // setError(error.message);
+		});
+	  }
     return (
         <div>
            <div className="hero min-h-screen bg-img  ">
@@ -84,7 +96,7 @@ const SignUp = () => {
 				<input type="text" {...register("photourl", { required: true })} name="photourl" id="" placeholder="Your Photo URL" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 text-black " />
                 {errors.photourl && <span className='text-lime-400'>Photo URL is required</span>}
 			</div>
-      <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
+      <button onClick={googleSignInHandler} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
 				<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
 			</svg>
