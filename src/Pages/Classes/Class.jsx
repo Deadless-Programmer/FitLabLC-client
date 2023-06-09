366fc3e5c1f6bad64512a23a8b077bac2d36410d
Deadless-Programmer <webtuhin@gmail.com>
2023-06-09 15:14:34 +0600

    button will be disabled set when available seats are 0

--- src/Pages/Classes/Class.jsx
+++ src/Pages/Classes/Class.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
 const Class = ({data}) => {
+    
     const {image, name, instructor, availableSeats, price, selectButton} = data;
+    const isAvailable = availableSeats > 0;
+    const isAdmin = true;
+  const cardStyle = {
+    backgroundColor: isAvailable ? 'white' : 'red',
+  };
     return (
         <div>
-           <div className=" content-center rounded-md shadow-md bg-gray-100 text-black">
+           <div style={cardStyle} className=" content-center rounded-md shadow-md bg-gray-100 text-black">
 	<img src={image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
 	<div className="flex flex-col justify-between p-6 space-y-8">
 		<div className="space-y-2">
         <p className="dark:text-gray-100">{name}</p>
-			<h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-			<p className="dark:text-gray-100">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
+			<h2 className="text-3xl font-semibold tracking-wide">{instructor}</h2>
+			<div className='flex justify-between'>
+            <p className="dark:text-gray-100">Available seats : {availableSeats} </p>
+            <p className="dark:text-gray-100">price : ${price} </p>
+            </div>
 		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900">Read more</button>
+		{/* <button disabled={!isAvailable || isAdmin} onClick={() => handleSelect(data)}>
+        {isAdmin ? 'Logged in as Admin' : 'Select'}
+      </button> */}
+        <button disabled={!isAvailable} className="btn bg-red-600 border-0 text-white hover:bg-lime-700 capitalize">{selectButton}</button>
 	</div>
 </div> 
         </div>
